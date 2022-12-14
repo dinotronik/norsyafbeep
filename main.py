4e36134a910b716fdec1463931cf040b1d499127
dinotronik <easonliewyeusen8816@gmail.com>
2022-12-14 13:20:30 +0800

    New

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from nextcord.ext import commands, tasks
 from itertools import cycle
 import nextcord
 import os
-import keep_alive
 
 description = "A bot dedicated to Area-51."
 
@@ -157,6 +156,5 @@ async def reload_error(ctx, error):
         await ctx.send("You do not have the permissions to use this command.")
 
 
-keep_alive.keep_alive()
 if __name__ == '__main__':
     bot.run(os.environ["DISCORD_TOKEN"])
